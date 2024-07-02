c937d912d948ffcbec32c170d93ab14bd5eee0f5
dhruv-baria <168161255+dhruv-baria@users.noreply.github.com>
2024-07-02 19:13:23 +0530

    secound commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import NewsItem from './NewsItem';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <Header />
+        <Routes>
+        <Route path="/" element = {<NewsItem key="start" country="in" category="general" />}exact />
+          <Route path="/general" element = {<NewsItem key="general" country="in" category="general" />}exact />
+          <Route path="/business" element ={ <NewsItem key="busines" country="in" category="business" />}exact />
+          <Route path="/entertainment" element = {<NewsItem key="entertainment" country="in" category="entertainment" />}exact />
+          <Route path="/health" element = {<NewsItem key="health" country="in" category="health" />}exact />
+          <Route path="/science" element = {<NewsItem key="science" country="in" category="science" />}exact />
+          <Route path="/sports" element = {<NewsItem key="sports" country="in" category="sports" />}exact />
+          <Route path="/technology" element = {<NewsItem key="technology" country="in" category="technology" />}exact />
+        </Routes>
+      </Router>
+
+
     </div>
   );
 }
